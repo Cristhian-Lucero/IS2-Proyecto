549cb29e4d1ee8f00650821db9aec1b527ee5e35
Cristhian-Lucero <cristhianlucero1@fpuna.edu.py>
2024-08-28 23:54:29 -0400

    1ra_V_Login
    Prueba de htmls

--- login/views.py
+++ login/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request, "login/login.html")
+
+def base(request):
+    return render(request, "login/base.html")
+
+def base2(request):
+    return render(request, "login/base2.html")
