03412d439c8547ea3a48852ece6a27977e6fedf9
Bestemming <melu098.inm@fpuna.edu.py>
2024-09-01 22:13:34 -0400

    #podrida

--- IS2_Proyecto/urls.py
+++ IS2_Proyecto/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('', include('login.urls')),
     path('', include('publicacion.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/',include('django.contrib.auth.urls')),
+    #path('accounts/',include('django.contrib.auth.urls')),
     path('publicaciones/', include('publicaciones.urls')),  # Incluye las URLs de publicaciones
     path('accounts/', include('allauth.urls')),
 ]
