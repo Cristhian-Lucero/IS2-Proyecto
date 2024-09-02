59d587cd89b6a940986ac3461db910e94c7c9207
Cristhian-Lucero <cristhianlucero1@fpuna.edu.py>
2024-09-02 12:50:46 -0400

    Ajuste plantilla Cuenta de Terceros

--- login/urls.py
+++ login/urls.py
@@ -4,8 +4,6 @@ from .views import *
 urlpatterns = [
     path('', inicio, name='inicio'),
     path('inicio/', inicio, name='inicio'),
-    path('base/', base, name='base'),
-    path('base2/', base2, name='base2'),
     path('logout/',exit,name='exit'),
     path('rol/', rol, name='rol' ),
 
--- login/views.py
+++ login/views.py
@@ -9,12 +9,7 @@ from django.contrib.auth import logout
 
 def inicio(request):
     return render(request, "login/inicio.html")
-@login_required
-def base(request):
-    return render(request, "login/base.html")
-@login_required
-def base2(request):
-    return render(request, "login/base2.html")
+
 
 def exit(request):
     logout(request)
