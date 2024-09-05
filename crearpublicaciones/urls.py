86ca147b25f7f02ef5f39d282c0bca27fcc943fb
Maria Lujan Melgarejo Acosta <melu098.inm@fpuna.edu.py>
2024-09-05 13:12:29 -0400

    Creacion de las paginas definitivas (1)
    Crear publicaciones
    Mis publicaciones
    Previsualizacion

---
+++ crearpublicaciones/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('crear/', views.crear_publicacion, name='crear_publicacion'),
+    path('previsualizar/<int:pk>/', views.previsualizar_publicacion, name='previsualizar_publicacion'),
+    path('mis-publicaciones/', views.mis_publicaciones, name='mis_publicaciones'),
+    path('modificar/<int:pk>/', views.modificar_publicacion, name='modificar_publicacion'),
+    path('eliminar/<int:pk>/', views.eliminar_publicacion, name='eliminar_publicacion'),
+]
--- crearpublicaciones/views.py
+++ crearpublicaciones/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from .models import Publicacion
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .forms import PublicacionForm
+
+def crear_publicacion(request):
+    if request.method == 'POST':
+        form = PublicacionForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('mis_publicaciones')  # Redirigir a la lista de publicaciones
+    else:
+        form = PublicacionForm()
+    return render(request, 'crearPublicacion.html', {'form': form})
+
+
+
+def previsualizar_publicacion(request, pk):
+    publicacion = Publicacion.objects.get(id=pk)
+    return render(request, 'previsualizacion.html', {'publicacion': publicacion})
+
+
+def mis_publicaciones(request):
+    publicaciones = Publicacion.objects.all()  # Solo las publicaciones del usuario
+    return render(request, 'misPublicaciones.html', {'publicaciones': publicaciones})
+
+
+def modificar_publicacion(request, pk):
+    publicacion = Publicacion.objects.get(id=pk)
+    if request.method == 'POST':
+        form = PublicacionForm(request.POST, request.FILES, instance=publicacion)
+        if form.is_valid():
+            form.save()
+            return redirect('mis_publicaciones')
+    else:
+        form = PublicacionForm(instance=publicacion)
+    return render(request, 'crearPublicacion.html', {'form': form})
+
+
+def eliminar_publicacion(request, pk):
+    publicacion = Publicacion.objects.get(id=pk)
+    publicacion.delete()
+    return redirect('mis_publicaciones')
