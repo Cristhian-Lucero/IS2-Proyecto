d72322e1a39a347622c2a77f1f239fd895f4ac87
Cristhian-Lucero <cristhianlucero1@fpuna.edu.py>
2024-09-08 21:42:05 -0400

    Documentado Signals

--- login/signals.py
+++ login/signals.py
@@ -4,6 +4,18 @@ from .models import Usuario, Rol, Categoria, UsuarioRolCategoria
 
 @receiver(post_save, sender=Usuario)
 def asignar_rol_suscriptor(sender, instance, created, **kwargs):
+    """
+    Asigna el rol 'Suscriptor' (con id=5) a todas las categorías cuando se crea un nuevo usuario.
+
+    Argumentos:
+        sender (Model class): El modelo que envió la señal, en este caso, 'Usuario'.
+        instance (Usuario): La instancia del modelo 'Usuario' que se acaba de crear.
+        created (bool): Valor booleano que indica si la instancia fue creada (True) o actualizada (False).
+        **kwargs: Parámetros adicionales.
+
+    Retorna:
+        None: La función no retorna un valor, pero crea registros en la tabla 'UsuarioRolCategoria'.
+    """
     if created:
         # Obtén el rol con id 5
         rol_suscriptor = Rol.objects.get(id=5)
