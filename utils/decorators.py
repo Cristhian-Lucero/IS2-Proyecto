9f3bff5153d44ecb8b8eabc3d08ed9407013aae1
Ivan-Paredes-Ozuna <ivanreneozuna@fpuna.edu.py>
2024-09-15 17:17:33 -0400

    .html de error por falta de permiso hecha

--- utils/decorators.py
+++ utils/decorators.py
@@ -3,6 +3,7 @@ from functools import wraps
 from django.http import HttpResponseForbidden
 from login.models import UsuarioRolCategoria, Permiso, Usuario
 from crearpublicaciones.models import Publicacion
+from django.shortcuts import render
 
 def check_permiso_categoria(permisos, categoria_id=None):
     def decorator(view_func):
@@ -10,6 +11,7 @@ def check_permiso_categoria(permisos, categoria_id=None):
         def _wrapped_view(request, *args, **kwargs):
             user = request.user
             if not user.is_authenticated:
+                return render(request, 'sin_permiso.html')
                 return HttpResponseForbidden("No tienes permiso para realizar esta acción")
             
 
@@ -24,7 +26,8 @@ def check_permiso_categoria(permisos, categoria_id=None):
 
                 # Si aún es None, retornar un error
                 if local_categoria_id is None:
-                    print(f"La categoria es ")
+                    
+                    return HttpResponseForbidden("No se proporcionó una categoría válida")
 
             try:
                 # Lógica para verificar los permisos del usuario en la categoría
@@ -33,6 +36,7 @@ def check_permiso_categoria(permisos, categoria_id=None):
                 
                 for permiso_nombre in permisos:
                     if not usuario_rol.rol.permisos.filter(nombre=permiso_nombre).exists():
+                        return render(request, 'sin_permiso.html')
                         return HttpResponseForbidden("No tienes permiso para realizar esta acción")
             except UsuarioRolCategoria.DoesNotExist:
                 return HttpResponseForbidden("No tienes un rol asignado en esta categoría")
@@ -47,6 +51,7 @@ def check_permiso_publicacion_nueva(permisos):
         def _wrapped_view(request, *args, **kwargs):
             user = request.user
             if not user.is_authenticated:
+                return render(request, 'sin_permiso.html')
                 return HttpResponseForbidden("No tienes permiso para realizar esta acción")
 
             local_publicacion_id = kwargs.get('publicacion_id')  # Intentar obtener desde kwargs
@@ -69,6 +74,7 @@ def check_permiso_publicacion_nueva(permisos):
                 
                 for permiso_nombre in permisos:
                     if not usuario_rol.rol.permisos.filter(nombre=permiso_nombre).exists():
+                        return render(request, 'sin_permiso.html')
                         return HttpResponseForbidden("No tienes permiso para realizar esta acción")
                     print(f"se tiene permiso para '{permiso_nombre}' en la categoria con id {local_categoria_id}")
             except UsuarioRolCategoria.DoesNotExist:
@@ -84,6 +90,7 @@ def check_permiso_publicacion_modificar(permisos):
         def _wrapped_view(request, *args, **kwargs):
             user = request.user
             if not user.is_authenticated:
+                return render(request, 'sin_permiso.html')
                 return HttpResponseForbidden("No tienes permiso para realizar esta acción")
             local_publicacion_id = kwargs.get('publicacion_id')
             if local_publicacion_id is None and len(args) > 1:
@@ -105,6 +112,7 @@ def check_permiso_publicacion_modificar(permisos):
                 
                 for permiso_nombre in permisos:
                     if not usuario_rol.rol.permisos.filter(nombre=permiso_nombre).exists():
+                        return render(request, 'sin_permiso.html')
                         return HttpResponseForbidden("No tienes permiso para realizar esta acción")
                         
             except UsuarioRolCategoria.DoesNotExist:
