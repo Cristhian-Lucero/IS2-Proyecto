e14abbaab55401313e7b8436fe9aa538cd3c0098
Cristhian-Lucero <cristhianlucero1@fpuna.edu.py>
2024-09-26 21:09:47 -0400

    Ajustes Tablero

---
+++ Tablero/urls.py
@@ -0,0 +1,13 @@
+"""
+URLs de la aplicación 'Tableros'.
+
+Este archivo define las rutas de URL para gestionar el tablero Kanban
+"""
+
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('index/',index, name='index'),
+]
+
