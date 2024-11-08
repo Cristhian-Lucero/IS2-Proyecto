67fc63f0458c02603ac6b07cee7d3ffea9815b30
DiegoNico3 <diegonicolasmorenosaldivar@gmail.com>
2024-11-08 14:49:00 -0300

    Actualizado test y documentacion

--- IS2_Proyecto/Test/test_reportes.py
+++ IS2_Proyecto/Test/test_reportes.py
@@ -1,20 +1,34 @@
-from django.test import TestCase
-from django.urls import reverse
+from django.test import TestCase, RequestFactory
+from Perfil.models import Usuario
+from login.models import Categoria, Rol, UsuarioRolCategoria
 from reporte.models import Reporte
 from django.contrib.auth.models import User
 from datetime import datetime
+from reporte.views import *
 
-class ReporteTestCase(TestCase):
+class ReporteViewTestCase(TestCase):
     fixtures = [
         'IS2_Proyecto/Test/fixtures/roles_fixture.json',
         'IS2_Proyecto/Test/fixtures/permisos_fixture.json',
         'IS2_Proyecto/Test/fixtures/categorias_fixture.json',
-        'IS2_Proyecto/Test/fixtures/users_fixture.json' 
+        'IS2_Proyecto/Test/fixtures/users_fixture.json'
     ]
 
     def setUp(self):
         # Crear un usuario y reportes de prueba
         self.usuario = User.objects.get(username='superuser')
+        self.factory = RequestFactory()
+        self.categoria = Categoria.objects.get(id=1)
+        self.rol_suscriptor = Rol.objects.get(id=5)
+        self.rol_admin = Rol.objects.get(id=1)
+        self.usuario_rol_categoria = UsuarioRolCategoria.objects.get(
+            usuario=Usuario.objects.get(user_id=self.usuario),
+            categoria=self.categoria,
+            rol = self.rol_suscriptor  # Establecer el rol predeterminado como Suscriptor
+        )
+        self.usuario_rol_categoria.rol = self.rol_admin
+        self.usuario_rol_categoria.save()
+        
         self.reporte1 = Reporte.objects.create(
             titulo=f'Mas leido - {datetime.now().strftime("%Y-%m-%d")}',
             html_content='<p>Contenido de prueba para reporte mas leido</p>',
@@ -30,57 +44,64 @@ class ReporteTestCase(TestCase):
             tipo='mas likeado'
         )
 
-    def test_mas_vistos(self):
-        url = reverse('masVistos')
-        self.client.login(username='superuser', password='superuser')
-        response = self.client.get(url)
+    def test_mas_vistos_view(self):
+        request = self.factory.get('/masVistos')
+        request.user = self.usuario
+        response = masVistos(request)
         self.assertEqual(response.status_code, 200)
 
-    def test_mas_likeados(self):
-        url = reverse('masLikeados')
-        self.client.login(username='superuser', password='superuser')
-        response = self.client.get(url)
+    def test_mas_likeados_view(self):
+        request = self.factory.get('/masLikeados')
+        request.user = self.usuario
+        response = masLikeados(request)
         self.assertEqual(response.status_code, 200)
 
-    def test_por_tiempo(self):
-        url = reverse('porTiempo')
-        self.client.login(username='superuser', password='superuser')
-        response = self.client.get(url)
+    def test_por_tiempo_view(self):
+        request = self.factory.get('/porTiempo')
+        request.user = self.usuario
+        response = porTiempo(request)
         self.assertEqual(response.status_code, 200)
 
-    def test_publicado_por_tiempo(self):
-        url = reverse('publicadoPorTiempo')
-        self.client.login(username='superuser', password='superuser')
-        response = self.client.get(url)
+    def test_publicado_por_tiempo_view(self):
+        request = self.factory.get('/publicadoPorTiempo')
+        request.user = self.usuario
+        response = publicadoPorTiempo(request)
         self.assertEqual(response.status_code, 200)
 
-    def test_promedio_revision(self):
-        url = reverse('promedioRevision')
-        self.client.login(username='superuser', password='superuser')
-        response = self.client.get(url)
+    def test_promedio_revision_view(self):
+        request = self.factory.get('/promedioRevision')
+        request.user = self.usuario
+        response = promedioRevision(request)
         self.assertEqual(response.status_code, 200)
 
-    def test_lista_reportes(self):
-        url = reverse('listaReportes')
-        self.client.login(username='superuser', password='superuser')
-        response = self.client.get(url)
+    def test_lista_reportes_view(self):
+        request = self.factory.get('/listaReportes')
+        request.user = self.usuario
+        response = listaReportes(request)
         self.assertEqual(response.status_code, 200)
 
-    def test_visualizacion_reporte(self):
-        url = reverse('visualizarReporte', args=[self.reporte1.id])
-        self.client.login(username='superuser', password='superuser')
-        response = self.client.get(url)
+    def test_visualizacion_reporte_view(self):
+        request = self.factory.get(f'/visualizarReporte/{self.reporte1.id}')
+        request.user = self.usuario
+        response = visualizarReporte(request, reporte_id=self.reporte1.id)
         self.assertEqual(response.status_code, 200)
 
-    def test_eliminacion_reporte(self):
-        url = reverse('eliminarReporte', args=[self.reporte1.id])
-        self.client.login(username='superuser', password='superuser')
-        response = self.client.post(url)
+    def test_eliminacion_reporte_view(self):
+        request = self.factory.post(f'/eliminarReporte/{self.reporte1.id}')
+        request.user = self.usuario
+        response = eliminarReporte(request, reporte_id=self.reporte1.id)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(Reporte.objects.count(), 1)
 
-    def test_dashboard(self):
-        url = reverse('dashboard')
-        self.client.login(username='superuser', password='superuser')
-        response = self.client.get(url)
+    def test_dashboard_view(self):
+        request = self.factory.get('/dashboard')
+        request.user = self.usuario
+        response = dashboard(request)
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_inactivosPorFecha_view(self):
+        request = self.factory.get('/inactivosPorFecha')
+        request.user = self.usuario
+        response = inactivosPorFecha(request)
         self.assertEqual(response.status_code, 200)
